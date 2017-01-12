28e9237c23c77ee9b7265467691452088e132647
Gaurav Sharma <grv7070@gmail.com>
2017-01-12 03:27:03 -0500

    Created the loadActivity Function, Worked with Intents, Learned to work with if/else for choosing Image, Title and background color for an activity based on button clicked. -Version2.0 -Stable

--- app/src/main/java/com/example/gaurav/fitorflab/DetailsActivity.java
+++ app/src/main/java/com/example/gaurav/fitorflab/DetailsActivity.java
@@ -1,9 +1,13 @@
 package com.example.gaurav.fitorflab;
 
+import android.graphics.Color;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,6 +21,36 @@ public class DetailsActivity extends AppCompatActivity {
 
         TextView exerciseTitle = (TextView)findViewById(R.id.titleView);
         ImageView exerciseImage = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.activity_details);
+
+        String exerciseT = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseTitle.setText(exerciseT);
+
+        if(exerciseT.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS))
+        {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.weight2x, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+
+
+        }
+        else if(exerciseT.equalsIgnoreCase(MainActivity.EXERCISE_YOGA))
+        {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus2x, getApplicationContext().getTheme()));
+            exerciseImage.setPadding(0,0,0,0);
+            exerciseImage.setScaleX((float)0.7);
+            exerciseImage.setScaleY((float)0.7);
+            mainBG.setPadding(25,0,25,0);
+            mainBG.setBackgroundColor(Color.parseColor("#FF5722"));
+
+
+        }
+        else
+        {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.heart2x, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+
+
+        }
 
 
     }
